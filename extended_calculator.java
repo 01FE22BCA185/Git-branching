c41d144bfbc5db8f367f7de313957c86d41aba25
01FE22BCA185 <01fe22bca185@kletech.ac.in>
2024-02-14 15:54:17 +0530

    Adding two files to branch

---
+++ extended_calculator.java
@@ -0,0 +1,15 @@
+public class Extended_Calculator {
+    float  square(float side){
+        return (side*side);
+     }
+     
+     float  cube(float side){
+        return (side*side*side);
+     }
+     float Modulus(float a,float b){
+        return (a%b);
+     }
+     double areaoftria(double base,double height){
+        return (0.5*base*height);
+     }
+}
